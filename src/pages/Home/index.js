8ed3a0465459c1dd1eb3feb753b7f194048e92c1
Bruno <brunofiliperamos@hotmail.com>
2019-09-02 19:53:00 -0300

    feat: default Home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Link } from './styles'
+
+const Home = () => (
+  <>
+    <Container>
+      <Link href='/characters'>Characters</Link>
+    </Container>
+  </>
+)
+
+export default Home
