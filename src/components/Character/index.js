f9ce6ce0a8c718140b19515f4adc94426f67711e
Bruno <brunofiliperamos@hotmail.com>
2019-09-02 19:52:30 -0300

    feat: default Character component

---
+++ src/components/Character/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Container } from './styles'
+const Character = ({ children }) => {
+  return (
+    <>
+      <Container>{children}</Container>
+    </>
+  )
+}
+
+export default Character
