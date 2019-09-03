effdf095ca0585d391311d1ace450469fb155baa
Bruno <brunofiliperamos@hotmail.com>
2019-09-03 20:31:04 -0300

    feat(Profile): add animation to Photo

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -18,11 +18,16 @@ export const StyledLink = styled(Link)``
 
 export const Photo = styled.img`
   height: 300px;
+  transition: transform .2s
+  &:hover {
+    transform: scale(1.2);
+  }
   @media (min-width: 320px) and (max-width: 425px) {
     width: 70%;
   }
 `
 export const Wrapper = styled.div`
+  margin-top: 30px;
   display: flex;
   justify-content: space-between;
   @media (min-width: 320px) and (max-width: 768px) {
