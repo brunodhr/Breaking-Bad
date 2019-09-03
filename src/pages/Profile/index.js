9e94dd3d0daa0338830cdeb0fd619370fa001465
Bruno <brunofiliperamos@hotmail.com>
2019-09-03 14:48:34 -0300

    feat(Profile): add responsive

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -8,30 +8,32 @@ const Profile = props => {
   const char = props.location.state.data
   return (
     <Layout showInput={false}>
-      <S.Container>
-        <img src={char.img} alt='' />
-        <h1>Nome</h1>
-        <h3>{char.name}</h3>
-        {char.birthday !== 'Unknown' ? (
-          <>
-            <h1>Data de nascimento</h1>
-            <h3>{char.birthday}</h3>
-          </>
-        ) : (
-          ''
-        )}
+      <S.Wrapper>
+        <S.Photo src={char.img} alt='' />
+        <S.Container>
+          <h1>Nome</h1>
+          <h3>{char.name}</h3>
+          {char.birthday !== 'Unknown' ? (
+            <>
+              <h1>Data de nascimento</h1>
+              <h3>{char.birthday}</h3>
+            </>
+          ) : (
+            ''
+          )}
 
-        <h1>Apelido</h1>
-        <h3>{char.nickname}</h3>
-        <h1>Profissões</h1>
-        <h3>{char.occupation.toString()}</h3>
-        <h1>Ator</h1>
-        <h3>{char.portrayed}</h3>
-        <h1>Status</h1>
-        <h3>{char.status === 'Alive' ? 'Vivo' : 'Morto'}</h3>
-        <h1>Temporadas em que apareceu</h1>
-        <h3>{char.appearance.toString()}</h3>
-      </S.Container>
+          <h1>Apelido</h1>
+          <h3>{char.nickname}</h3>
+          <h1>Profissões</h1>
+          <h3>{char.occupation.toString()}</h3>
+          <h1>Ator</h1>
+          <h3>{char.portrayed}</h3>
+          <h1>Status</h1>
+          <h3>{char.status === 'Alive' ? 'Vivo' : 'Morto'}</h3>
+          <h1>Temporadas em que apareceu</h1>
+          <h3>{char.appearance.toString()}</h3>
+        </S.Container>
+      </S.Wrapper>
     </Layout>
   )
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,5 +1,32 @@
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  h1 {
+    color: #61dbfb;
+  }
+  @media (max-width: 1024px) {
+    h1 {
+      font-size: 20px;
+    }
+    h3 {
+      font-size: 15px;
+    }
+  }
+`
 export const StyledLink = styled(Link)``
+
+export const Photo = styled.img`
+  height: 300px;
+  @media (min-width: 320px) and (max-width: 425px) {
+    width: 70%;
+  }
+`
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  @media (min-width: 320px) and (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`
