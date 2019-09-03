d9de14731af9f18a1475d2bf3c39b7d6b3287316
Bruno <brunofiliperamos@hotmail.com>
2019-09-03 00:42:15 -0300

    feat(character): first version component

--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -1,11 +1,51 @@
 import React from 'react'
-import { Container } from './styles'
-const Character = ({ children }) => {
+import PropTypes from 'prop-types'
+import star from 'star.svg'
+
+import * as S from './styles'
+
+const Character = ({ img, name, birthday, occupation, status }) => {
   return (
-    <>
-      <Container>{children}</Container>
-    </>
+    <S.Container>
+      <S.Card to='/character/:id'>
+        <S.Status status={status}>
+          {status === 'Alive' ? 'Vivo' : 'Morto'}
+        </S.Status>
+        <S.Banner src={img} />
+        <S.Name>{name}</S.Name>
+      </S.Card>
+      <S.Infos>
+        {birthday !== 'Unknown' ? (
+          <S.BirthdayBox>
+            <S.StyledStar src={star} alt='' />
+            <S.Birthday>{birthday !== 'Unknown' ? birthday : ''}</S.Birthday>
+          </S.BirthdayBox>
+        ) : (
+          ''
+        )}
+
+        <S.Occupations>
+          {occupation.map(o => (
+            <>{o}, </>
+          ))}
+        </S.Occupations>
+      </S.Infos>
+    </S.Container>
   )
+}
+
+Character.defaultProps = {
+  children: '',
+  img: '',
+  name: '',
+  birthday: ''
+}
+
+Character.propTypes = {
+  children: PropTypes.string,
+  img: PropTypes.string,
+  name: PropTypes.string,
+  birthday: PropTypes.string
 }
 
 export default Character
--- src/components/Character/styles.js
+++ src/components/Character/styles.js
@@ -1,3 +1,74 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 25%;
+  @media (min-width: 320px) and (max-width: 425px) {
+    width: 100%;
+  }
+  @media (min-width: 426px) and (max-width: 1024px) {
+    width: 50%;
+  }
+`
+export const Card = styled(Link)`
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const Banner = styled.img`
+  height: 300px;
+  width: 100%;
+`
+
+export const Name = styled.span`
+  font-size: 18px;
+  font-weight: 700;
+  position: absolute;
+  left: 5%;
+  bottom: 10px;
+  color: #fff;
+`
+
+export const BirthdayBox = styled.div`
+  padding: 5px;
+`
+
+export const Status = styled.span`
+  background-color: ${props =>
+    props.status === 'Alive' ? '#0D7842' : '#F56624'};
+  border-radius: 5px 0 0 5px;
+  position: absolute;
+  right: 0;
+  top: 10px;
+  font-size: 14px;
+  padding: 10px 30px;
+  color: #fff;
+`
+export const Birthday = styled.span`
+  font-size: 14px;
+  padding: 10px;
+`
+export const Occupations = styled.span`
+  font-size: 13px;
+  color: #505050;
+`
+
+export const Infos = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+`
+
+export const StyledStar = styled.img`
+  width: 15px;
+  height: 15px;
+  background-color: blue;
+  fill: red !important;
+  stroke: red;
+  color: red;
+`
