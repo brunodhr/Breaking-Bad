f068841b3d1cd6517050427557945875e357cec3
Bruno <brunofiliperamos@hotmail.com>
2019-09-04 00:13:16 -0300

    feat(Character): handle spoiler

--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 import star from 'star.svg'
 
 import * as S from './styles'
 
 const Character = ({ data }) => {
+  const spoiler = useSelector(state => state.spoiler.spoiler)
   return (
     <S.Container>
       <S.Card to={{ pathname: `/character/${data.char_id}`, state: { data } }}>
-        <S.Status status={data.status}>
-          {data.status === 'Alive' ? 'Vivo' : 'Morto'}
-        </S.Status>
+        {spoiler ? (
+          <S.Status status={data.status}>
+            {data.status === 'Alive' ? 'Vivo' : 'Morto'}
+          </S.Status>
+        ) : (
+          ''
+        )}
         <S.Banner src={data.img} />
         <S.Name>{data.name}</S.Name>
       </S.Card>
